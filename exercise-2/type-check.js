6f9562e9410cd84cc582b7498269d04dfb2fdcf0
Albert Krasniqi <albert.krasniqi@groupetrefle.com>
2018-11-27 14:54:51 +0100

    ajout verif

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -9,7 +9,24 @@ function type_check_v1(arg1, arg2){
     }else if( Array.isArray(arg1) ){
         return true;
     }
-    
     return false;
 
 }
+
+var myUndefined = undefined;
+
+function myFunction(){
+    console.log('test');
+}
+
+type_check_v1(1,"number");
+type_check_v1("string","number");
+type_check_v1(true,"number");
+type_check_v1({"prop1": 1},"object");
+type_check_v1(null,"null");
+type_check_v1(null,"object");
+type_check_v1(myUndefined,"undefined");
+type_check_v1("string","string");
+type_check_v1([1,2,3],"array");
+type_check_v1(myFunction,"function");
+type_check_v1([1,2,3],"object");
