33258d81d40027f8196c1b46e09327be06e47b2a
Albert Krasniqi <albert.krasniqi@groupetrefle.com>
2018-11-27 14:47:01 +0100

    correction verif array

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -6,13 +6,10 @@ function type_check_v1(arg1, arg2){
         return true;
     } else if( typeof arg1 === 'undefined' ){
         return true;
-    }else if( isArray(arg1) ){
+    }else if( Array.isArray(arg1) ){
         return true;
     }
     
     return false;
 
 }
-
-
-type_check_v1(1, 'number');
